3c6137fb845412607851db8cf832c537f584ffc3
Will Roberts <willroberts3@protonmail.com>
2024-02-05 08:53:20 -0500

    Adds reader tests

--- readers.go
+++ readers.go
@@ -11,18 +11,18 @@ func readBytes(n uint, r io.Reader) ([]byte, error) {
 	return buf, err
 }
 
-func readUint32(r io.Reader) (uint32, error) {
-	buf := make([]byte, 4)
-	if _, err := io.ReadFull(r, buf); err != nil {
-		return 0, err
-	}
-	return binary.LittleEndian.Uint32(buf), nil
-}
-
 func readUint16(r io.Reader) (uint16, error) {
 	buf := make([]byte, 2)
 	if _, err := io.ReadFull(r, buf); err != nil {
 		return 0, err
 	}
 	return binary.LittleEndian.Uint16(buf), nil
+}
+
+func readUint32(r io.Reader) (uint32, error) {
+	buf := make([]byte, 4)
+	if _, err := io.ReadFull(r, buf); err != nil {
+		return 0, err
+	}
+	return binary.LittleEndian.Uint32(buf), nil
 }
--- readers_test.go
+++ readers_test.go
@@ -1,1 +1,71 @@
 package modutils
+
+import (
+	"bytes"
+	"encoding/binary"
+	"testing"
+)
+
+func TestReadBytes(t *testing.T) {
+	b := []byte{0, 1, 2, 3}
+	r := bytes.NewReader(b)
+	_, err := readBytes(4, r)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestReadBytesOversized(t *testing.T) {
+	b := []byte{0, 1, 2}
+	r := bytes.NewReader(b)
+	_, err := readBytes(4, r)
+	if err == nil {
+		t.Error("failed to detect error when reading too many bytes")
+	}
+}
+
+func TestReadUint16(t *testing.T) {
+	in := uint16(1234)
+	b := make([]byte, 2)
+	binary.LittleEndian.PutUint16(b, in)
+	r := bytes.NewReader(b)
+	out, err := readUint16(r)
+	if err != nil {
+		t.Error(err)
+	}
+	if in != out {
+		t.Error("expected:", in, "got:", out)
+	}
+}
+
+func TestReadUint16Oversized(t *testing.T) {
+	b := make([]byte, 1)
+	r := bytes.NewReader(b)
+	_, err := readUint16(r)
+	if err == nil {
+		t.Error("failed to detect error when reading too many bytes")
+	}
+}
+
+func TestReadUint32(t *testing.T) {
+	in := uint32(1234)
+	b := make([]byte, 4)
+	binary.LittleEndian.PutUint32(b, in)
+	r := bytes.NewReader(b)
+	out, err := readUint32(r)
+	if err != nil {
+		t.Error(err)
+	}
+	if in != out {
+		t.Error("expected:", in, "got:", out)
+	}
+}
+
+func TestReadUint32Oversized(t *testing.T) {
+	b := make([]byte, 3)
+	r := bytes.NewReader(b)
+	_, err := readUint32(r)
+	if err == nil {
+		t.Error("failed to detect error when reading too many bytes")
+	}
+}
